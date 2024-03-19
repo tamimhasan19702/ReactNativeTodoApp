06a9d2996cfb426235407d44070cae077f7dc946
tamimhasan197020 <tamimhasancu@gmail.com>
2024-03-20 00:51:29 +0600

    updated the login and register field options

--- app/(auth)/login.js
+++ app/(auth)/login.js
@@ -2,6 +2,7 @@
 
 import {
   KeyboardAvoidingView,
+  Pressable,
   SafeAreaView,
   StyleSheet,
   Text,
@@ -10,10 +11,12 @@ import {
 } from "react-native";
 import React, { useState } from "react";
 import { MaterialIcons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
 
 const login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const router = useRouter();
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.loginView}>
@@ -58,6 +61,35 @@ const login = () => {
             <Text>Kepp me Logged in</Text>
             <Text style={{ color: "#007fff" }}>Forgot Password</Text>
           </View>
+
+          <View style={{ marginTop: 60 }}>
+            <Pressable
+              style={{
+                width: 200,
+                backgroundColor: "#6699cc",
+                padding: 15,
+                borderRadius: 6,
+                marginLeft: "auto",
+                marginRight: "auto",
+              }}>
+              <Text style={styles.buttonText}>Login</Text>
+            </Pressable>
+
+            <Text style={styles.signUptext}>
+              Don't have an account?
+              <Pressable onPress={() => router.push("/register")}>
+                <Text
+                  style={{
+                    color: "#007fff",
+                    marginLeft: 8,
+                    textAlign: "center",
+                    fontSize: 18,
+                  }}>
+                  Sign up
+                </Text>
+              </Pressable>
+            </Text>
+          </View>
         </View>
       </KeyboardAvoidingView>
     </SafeAreaView>
@@ -71,6 +103,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
+    justifyContent: "start",
   },
   loginView: {
     marginTop: 80,
@@ -110,5 +143,20 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginTop: 12,
     justifyContent: "space-between",
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 16,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  signUptext: {
+    marginTop: 20,
+    textAlign: "center",
+    fontSize: 18,
+    color: "gray",
   },
 });
--- app/(auth)/register.js
+++ app/(auth)/register.js
@@ -1,16 +1,162 @@
 /** @format */
 
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  KeyboardAvoidingView,
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import { MaterialIcons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
 
 const register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
   return (
-    <View>
-      <Text>register</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.loginView}>
+        <Text style={styles.loginText}>TODO LIST TRACKER</Text>
+      </View>
+      <KeyboardAvoidingView>
+        <View style={styles.loginAvoidView}>
+          <Text style={styles.loginAvoidText}>Register To you account</Text>
+        </View>
+
+        <View style={styles.textInputContainer}>
+          <View style={styles.textInputView}>
+            <MaterialIcons
+              name="email"
+              size={24}
+              color="Gray"
+              style={{ marginLeft: 10 }}
+            />
+            <TextInput
+              style={[styles.textInput, { fontSize: email ? 17 : 17 }]}
+              value={email}
+              onChangeText={setEmail}
+              placeholder="Enter your Email"
+            />
+          </View>
+          <View style={styles.textInputView}>
+            <MaterialIcons
+              name="lock"
+              size={24}
+              color="Gray"
+              style={{ marginLeft: 10 }}
+            />
+            <TextInput
+              style={[styles.textInput, { fontSize: email ? 17 : 17 }]}
+              value={password}
+              onChangeText={setPassword}
+              placeholder="Password"
+            />
+          </View>
+
+          <View style={styles.keepLoginView}>
+            <Text>Kepp me Logged in</Text>
+            <Text style={{ color: "#007fff" }}>Forgot Password</Text>
+          </View>
+
+          <View style={{ marginTop: 60 }}>
+            <Pressable
+              style={{
+                width: 200,
+                backgroundColor: "#6699cc",
+                padding: 15,
+                borderRadius: 6,
+                marginLeft: "auto",
+                marginRight: "auto",
+              }}>
+              <Text style={styles.buttonText}>Login</Text>
+            </Pressable>
+
+            <Text style={styles.signUptext}>
+              Already have an account?
+              <Pressable onPress={() => router.push("/login")}>
+                <Text
+                  style={{
+                    color: "#007fff",
+                    marginLeft: 8,
+                    textAlign: "center",
+                    fontSize: 18,
+                  }}>
+                  Sign in
+                </Text>
+              </Pressable>
+            </Text>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 };
 
 export default register;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "start",
+  },
+  loginView: {
+    marginTop: 80,
+  },
+  loginText: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: "#0066b2",
+  },
+  loginAvoidView: {
+    alignItems: "center",
+  },
+  loginAvoidText: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginTop: 20,
+  },
+  textInputContainer: {
+    marginTop: 70,
+  },
+  textInputView: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 5,
+    backgroundColor: "#E0E0E0",
+    paddingVertical: 5,
+    borderRadius: 5,
+    marginTop: 30,
+  },
+  textInput: {
+    color: "gray",
+    marginVertical: 10,
+    width: 300,
+  },
+  keepLoginView: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 12,
+    justifyContent: "space-between",
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 16,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  signUptext: {
+    marginTop: 20,
+    textAlign: "center",
+    fontSize: 18,
+    color: "gray",
+  },
+});
