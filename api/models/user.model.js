e1242148c44943bafae602b9c6880bfbe1283fea
tamimhasan197020 <tamimhasancu@gmail.com>
2024-03-20 21:35:02 +0600

    two modal schema user and todo created

---
+++ api/models/user.model.js
@@ -0,0 +1,33 @@
+/** @format */
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  todos: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Todo",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
