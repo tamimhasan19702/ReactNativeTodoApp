a04bf26a0384be50a0eee5103be4fd23c6ce4efa
tamimhasan197020 <tamimhasancu@gmail.com>
2024-03-22 21:48:56 +0600

    updated the bottom modal

--- app/(auth)/login.js
+++ app/(auth)/login.js
@@ -91,6 +91,7 @@ const Login = () => {
               value={password}
               onChangeText={setPassword}
               placeholder="Password"
+              secureTextEntry
             />
           </View>
 
--- app/(tabs)/home/index.js
+++ app/(tabs)/home/index.js
@@ -7,14 +7,22 @@ import {
   Text,
   View,
   TouchableOpacity,
+  TextInput,
 } from "react-native";
 import React, { useState } from "react";
-import { AntDesign } from "@expo/vector-icons";
+import { AntDesign, Ionicons } from "@expo/vector-icons";
 import { Image } from "react-native";
+import {
+  BottomModal,
+  ModalContent,
+  ModalTitle,
+  SlideAnimation,
+} from "react-native-modals";
 
 const index = () => {
   const todos = [];
   const [isVisible, setIsVisible] = useState(false);
+  const [todo, setTodo] = useState({});
   return (
     <>
       <View style={styles.container}>
@@ -27,7 +35,7 @@ const index = () => {
         <TouchableOpacity style={[styles.press, { marginRight: "auto" }]}>
           <Text style={styles.text}>Personal</Text>
         </TouchableOpacity>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => setIsVisible(!isVisible)}>
           <AntDesign name="pluscircle" size={30} color="#007fff" />
         </TouchableOpacity>
       </View>
@@ -47,13 +55,51 @@ const index = () => {
               <Text style={styles.noTodoText}>
                 No Task added for today!! add a task
               </Text>
-              <TouchableOpacity>
+              <TouchableOpacity onPress={() => setIsVisible(!isVisible)}>
                 <AntDesign name="pluscircle" size={30} color="#007fff" />
               </TouchableOpacity>
             </View>
           )}
         </View>
       </ScrollView>
+
+      <BottomModal
+        onBackDropPress={() => setIsVisible(isVisible)}
+        onHardwareBackPress={() => setIsVisible(isVisible)}
+        swipeDirection={["up", "down"]}
+        swipeThreshold={200}
+        modalTitle={<ModalTitle title="Add a Todo Task" />}
+        modalAnimation={
+          new SlideAnimation({
+            slideFrom: "bottom",
+          })
+        }
+        visible={isVisible}
+        onTouchOutside={() => setIsVisible(isVisible)}>
+        <ModalContent style={{ width: "100%", height: 200 }}>
+          <View
+            style={{
+              marginVertical: 10,
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 10,
+            }}>
+            <TextInput
+              value={todo}
+              onChangeText={(text) => setTodo(text)}
+              placeholder="Enter a new Todo here"
+              style={{
+                padding: 10,
+                borderColor: "#7cb9e8",
+                borderWidth: 1.5,
+                borderRadius: 5,
+                flex: 1,
+              }}
+            />
+            <Ionicons name="send" size={28} color="#7cb9e8" />
+          </View>
+        </ModalContent>
+      </BottomModal>
     </>
   );
 };
